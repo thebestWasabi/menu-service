d09e9d21634371f33ce89edb7ea365d3caf86298
wasabi <openpw@icloud.com>
2025-03-10 00:05:47 +0300

    add: MenuServiceImpl & MenuServiceImplTest

--- src/test/java/ru/javaops/cloudjava/menuservice/service/impl/MenuServiceImplTest.java
+++ src/test/java/ru/javaops/cloudjava/menuservice/service/impl/MenuServiceImplTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import ru.javaops.cloudjava.menuservice.BaseTest;
 import ru.javaops.cloudjava.menuservice.dto.MenuItemDto;
 import ru.javaops.cloudjava.menuservice.dto.SortBy;
+import ru.javaops.cloudjava.menuservice.exception.MenuServiceException;
 import ru.javaops.cloudjava.menuservice.service.MenuService;
 import ru.javaops.cloudjava.menuservice.storage.model.Category;
 import ru.javaops.cloudjava.menuservice.storage.repositories.MenuItemRepository;
@@ -15,6 +16,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.junit.Assert.assertThrows;
 
 @SpringBootTest
 public class MenuServiceImplTest extends BaseTest {
@@ -25,10 +27,37 @@ public class MenuServiceImplTest extends BaseTest {
     private MenuItemRepository repository;
 
     @Test
+    void getMenu_returnsMenu_whenMenuInDb() {
+        var id = getIdByName("Cappuccino");
+        var menu = menuService.getMenu(id);
+        assertThat(menu).isNotNull();
+        assertThat(menu.getName()).isEqualTo("Cappuccino");
+        assertThat(menu.getId()).isNotNull();
+        assertThat(menu.getCreatedAt()).isNotNull();
+        assertThat(menu.getUpdatedAt()).isNotNull();
+    }
+
+    @Test
+    void getMenu_throws_whenNoMenuInDb() {
+        assertThrows(
+                MenuServiceException.class,
+                () -> menuService.getMenu(1000L)
+        );
+    }
+
+    @Test
     void getMenusFor_DRINKS_returnsCorrectList() {
         List<MenuItemDto> drinks = menuService.getMenusFor(Category.DRINKS, SortBy.AZ);
         assertThat(drinks).hasSize(3);
         assertElementsInOrder(drinks, MenuItemDto::getName, List.of("Cappuccino", "Tea", "Wine"));
+    }
+
+    @Test
+    void deleteMenuItem_deletesItem() {
+        var id = getIdByName("Cappuccino");
+        menuService.deleteMenuItem(id);
+        var deletedOpt = repository.findById(id);
+        assertThat(deletedOpt).isEmpty();
     }
 
     @Test
@@ -43,5 +72,42 @@ public class MenuServiceImplTest extends BaseTest {
         assertFieldsEquality(result, dto, "name", "description", "price", "imageUrl", "timeToCook");
         assertThat(result.getCreatedAt()).isAfter(now);
         assertThat(result.getUpdatedAt()).isAfter(now);
+    }
+
+    @Test
+    void createMenuItem_throwsWhenItemWithThatNameExists() {
+        var dto = TestData.createMenuRequest();
+        dto.setName("Cappuccino");
+        assertThrows(
+                MenuServiceException.class,
+                () -> menuService.createMenuItem(dto)
+        );
+    }
+
+    @Test
+    void updateMenuItem_updatesMenuItem_whenItemPresentInDb() {
+        var id = getIdByName("Cappuccino");
+        var update = TestData.updateMenuFullRequest();
+        MenuItemDto updated = menuService.updateMenuItem(id, update);
+        assertFieldsEquality(updated, update, "name", "description", "price", "timeToCook", "imageUrl");
+    }
+
+    @Test
+    void updateMenuItem_throws_whenNoItemInDb() {
+        var id = 1000L;
+        var update = TestData.updateMenuFullRequest();
+        assertThrows(
+                MenuServiceException.class,
+                () -> menuService.updateMenuItem(id, update)
+        );
+    }
+
+    @Test
+    void updateMenuItem_throws_whenUpdateRequestContainsNotUniqueName() {
+        var id = getIdByName("Cappuccino");
+        var update = TestData.updateMenuFullRequest();
+        update.setName("Wine");
+        assertThrows(MenuServiceException.class,
+                () -> menuService.updateMenuItem(id, update));
     }
 }
